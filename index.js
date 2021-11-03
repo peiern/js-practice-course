cb969728eda273eb0e7103f37aa0d59e35b30946
Pei Ern Koh <peiern.28@gmail.com>
2021-11-03 19:00:10 +0800

    initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+let bonusPoints = 50
+
+bonusPoints = bonusPoints * 2
+console.log(bonusPoints);
+
+bonusPoints = (bonusPoints / 2) / 2
+console.log(bonusPoints);
+
+bonusPoints = bonusPoints + 50
+console.log(bonusPoints);
