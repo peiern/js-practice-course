bf58875178c516a793c71fc930310cda9e3be1ed
Pei Ern Koh <peiern.28@gmail.com>
2021-11-15 19:05:14 +0800

    added some css, able to start game

--- building-blackjack/index.js
+++ building-blackjack/index.js
@@ -1,9 +1,29 @@
-// 1. Create two variables, firstCard and secondCard.
-// Set their values to a random number between 2-11
+const messageEl = document.querySelector("#message-el")
+const sumEl = document.querySelector("#sum-el")
+const cardsEl = document.querySelector("#cards-el")
+let hasBlackJack = false
+let isAlive = true
+let message = ""
 
 let firstCard = Math.floor(Math.random() * 12)
 let secondCard = Math.floor(Math.random() * 12)
-
-// 2. Create a variable, sum, and set it to the sum of the two cards
 let sum = firstCard + secondCard
 
+function startGame(){
+  cardsEl.textContent = "Cards: " + firstCard + " " + secondCard
+  sumEl.textContent = "Sum: " + sum
+  if (sum <= 20) {
+    message = "Do you want to draw a new card?"
+  } else if (sum === 21) {
+    message = "You've got Blackjack!"
+    hasBlackJack = true
+  } else {
+    message = "You're out of the game!"
+    isAlive = false
+  }
+  messageEl.textContent = message;
+};
+
+function newCard() {
+  console.log("Drawing new card");
+}
