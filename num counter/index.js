9016b2f117a38c96db5c0c6989e81f6146602933
Pei Ern Koh <peiern.28@gmail.com>
2021-11-15 17:35:45 +0800

    able to add, subtract, divide and multiply

--- num counter/index.js
+++ num counter/index.js
@@ -2,10 +2,29 @@ let num1 = 8
 let num2 = 2
 document.getElementById("num1-el").textContent = num1
 document.getElementById("num2-el").textContent = num2
+let sum = document.getElementById("sum-el")
 
 // create 4 functions: add(), subtract(), divide(), multiply()
 // call the correct function when the user cicks on one of the buttons
 // perform the given calculation using num1 and num2
 // render the result of the calculation in the paragraph with id="sum-el"
 
+function add() {
+  const addResult = num1 + num2
+  sum.textContent = "Sum: " + addResult
+}
 
+function subtract() {
+  const subtractResult = num1 - num2
+  sum.textContent = "Sum: " + subtractResult
+}
+
+function divide() {
+  const divideResult = num1 / num2
+  sum.textContent = "Sum: " + divideResult
+}
+
+function multiply() {
+  const multiplyResult = num1 * num2
+  sum.textContent = "Sum: " + multiplyResult
+}
