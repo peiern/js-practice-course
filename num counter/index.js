7793559bad6f36fc8ecbb6607653dc4d51a27005
Pei Ern Koh <peiern.28@gmail.com>
2021-11-15 17:24:57 +0800

    added css file

---
+++ num counter/index.js
@@ -0,0 +1,11 @@
+let num1 = 8
+let num2 = 2
+document.getElementById("num1-el").textContent = num1
+document.getElementById("num2-el").textContent = num2
+
+// create 4 functions: add(), subtract(), divide(), multiply()
+// call the correct function when the user cicks on one of the buttons
+// perform the given calculation using num1 and num2
+// render the result of the calculation in the paragraph with id="sum-el"
+
+
