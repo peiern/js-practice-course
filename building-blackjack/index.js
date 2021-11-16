0969797d2fee5c08029bb67c72791bbf078a1958
Pei Ern Koh <peiern.28@gmail.com>
2021-11-16 17:47:27 +0800

    completed startGame function

--- building-blackjack/index.js
+++ building-blackjack/index.js
@@ -2,20 +2,29 @@ const messageEl = document.querySelector("#message-el")
 const sumEl = document.querySelector("#sum-el")
 const cardsEl = document.querySelector("#cards-el")
 let hasBlackJack = false
-let isAlive = true
+let isAlive = false
 let message = ""
-
-let firstCard = getRandomCard()
-let secondCard = getRandomCard()
-let cards = [firstCard, secondCard]
-let sum = firstCard + secondCard
+let cards = []
+let sum = 0
 
 function getRandomCard(){
-  return Math.floor(Math.random() * 12)
+  const randomNumber = Math.floor(Math.random() * 13) + 1
+  if (randomNumber > 10){
+    return 1
+  } else if (randomNumber === 1){
+    return 11
+  } else {
+    return randomNumber
+  }
 }
 
-
 function startGame() {
+  isAlive = true
+  let firstCard = getRandomCard()
+  let secondCard = getRandomCard()
+
+  cards = [firstCard, secondCard]
+  sum = firstCard + secondCard
   renderGame()
 }
 
