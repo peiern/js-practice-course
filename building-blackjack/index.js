6b3650d82a4a07e2b83da74043217202dfb51920
Pei Ern Koh <peiern.28@gmail.com>
2021-11-16 17:19:22 +0800

    added for loop for cards array

--- building-blackjack/index.js
+++ building-blackjack/index.js
@@ -15,7 +15,11 @@ function startGame() {
 }
 
 function renderGame(){
-  cardsEl.textContent = "Cards: " + cards[0] + " " + cards[1]
+  cardsEl.textContent = "Cards: "
+  for (let i = 0; i < cards.length; i++) {
+    cardsEl.textContent += cards[i] + " "
+  }
+
   sumEl.textContent = "Sum: " + sum
   if (sum <= 20) {
     message = "Do you want to draw a new card?"
@@ -30,8 +34,8 @@ function renderGame(){
 };
 
 function newCard() {
-  console.log("Drawing new card");
   const card = 3
   sum += card
+  cards.push(card)
   renderGame()
 }
