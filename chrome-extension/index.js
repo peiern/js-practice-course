601866a90ae5ba69623a4a87a4ab8db81e8a1eca
Pei Ern Koh <peiern.28@gmail.com>
2021-11-17 16:37:08 +0800

    refactored renderLeads function

--- chrome-extension/index.js
+++ chrome-extension/index.js
@@ -1,4 +1,5 @@
 let myLeads = []
+let oldLeads = []
 const inputEL = document.querySelector("#input-el")
 const inputBtn = document.querySelector("#input-btn")
 const ulEl = document.querySelector("#ul-el")
@@ -7,29 +8,29 @@ const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
 
 if (leadsFromLocalStorage) {
   myLeads = leadsFromLocalStorage
-  renderLeads()
+  render(myLeads)
 }
 
 deleteBtn.addEventListener('dblclick', function(){
   localStorage.clear()
   myLeads = ""
-  renderLeads()
+  render(myLeads)
 })
 
 inputBtn.addEventListener('click', function(){
   myLeads.push(inputEL.value)
   inputEL.value = ""
   localStorage.setItem('myLeads', JSON.stringify(myLeads))
-  renderLeads()
+  render(myLeads)
 })
 
-function renderLeads() {
+function render(leads) {
   let listItem = ""
-  for (let i = 0; i < myLeads.length; i++) {
+  for (let i = 0; i < leads.length; i++) {
     listItem += `
     <li>
-      <a href='${myLeads[i]}' target='_blank'>
-      ${myLeads[i]}</a>
+      <a href='${leads[i]}' target='_blank'>
+      ${leads[i]}</a>
     </li>
     `
   }
