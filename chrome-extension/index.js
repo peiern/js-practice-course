712d6d2073383d993104da194fe156bc7d5d46fa
Pei Ern Koh <peiern.28@gmail.com>
2021-11-17 17:04:56 +0800

    added permission for tabs and chrome api

--- chrome-extension/index.js
+++ chrome-extension/index.js
@@ -1,28 +1,15 @@
 let myLeads = []
-let oldLeads = []
 const inputEL = document.querySelector("#input-el")
 const inputBtn = document.querySelector("#input-btn")
 const ulEl = document.querySelector("#ul-el")
 const deleteBtn = document.querySelector("#delete-btn")
+const tabBtn = document.querySelector("#tab-btn")
 const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
 
 if (leadsFromLocalStorage) {
   myLeads = leadsFromLocalStorage
   render(myLeads)
 }
-
-deleteBtn.addEventListener('dblclick', function(){
-  localStorage.clear()
-  myLeads = ""
-  render(myLeads)
-})
-
-inputBtn.addEventListener('click', function(){
-  myLeads.push(inputEL.value)
-  inputEL.value = ""
-  localStorage.setItem('myLeads', JSON.stringify(myLeads))
-  render(myLeads)
-})
 
 function render(leads) {
   let listItem = ""
@@ -37,3 +24,24 @@ function render(leads) {
 
   ulEl.innerHTML = listItem
 }
+
+tabBtn.addEventListener('click', function(){
+  chrome.tabs.query({ active: true, lastFocusedWindow: true }, tabs => {
+    myLeads.push(tabs[0].url)
+    localStorage.setItem('myLeads', JSON.stringify(myLeads))
+    render(myLeads)
+  })
+})
+
+deleteBtn.addEventListener('dblclick', function(){
+  localStorage.clear()
+  myLeads = ""
+  render(myLeads)
+})
+
+inputBtn.addEventListener('click', function(){
+  myLeads.push(inputEL.value)
+  inputEL.value = ""
+  localStorage.setItem('myLeads', JSON.stringify(myLeads))
+  render(myLeads)
+})
